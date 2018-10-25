bf5674af3c4c56772ae60815dec8879da726b3f0
Sun <38485966+LeSun54@users.noreply.github.com>
2018-10-25 20:32:39 +0800

    Add files via upload

---
+++ Python Fundatmentals/Some boring test.ipynb.py
@@ -0,0 +1,83 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "I love the world\n",
+      "I love my new self\n",
+      "I will be one of the best programmer / machine learning expert / data scientist in the world\n"
+     ]
+    }
+   ],
+   "source": [
+    "print (\"I love the world\") \n",
+    "print(\"I love my new self\")\n",
+    "print (\"I will be one of the best programmer / machine learning expert / data scientist in the world\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Leon will be the best in the world!\n",
+      "Leon will be a great programmer and scientist!\n",
+      "Leon will be a professional\n"
+     ]
+    }
+   ],
+   "source": [
+    "class Leon:\n",
+    "    def __init__ (self, name, dream, career):\n",
+    "        self.name = name\n",
+    "        self.dream = dream\n",
+    "        self.career = career\n",
+    "    def statement (self):\n",
+    "        print (self.name + \" will be the best in the world!\")\n",
+    "        print (self.name + \" will be a great programmer and scientist!\")\n",
+    "        print (self.name + \" will be a professional\")\n",
+    "    \n",
+    "r1 = Leon (\"Leon\", \"programmer & scientist\", \"professional\")\n",
+    "r1.statement()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
